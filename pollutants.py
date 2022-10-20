4d5111385eeb7c491b2863d7ef38a056ddd5ccd8
vaneesa-writes <laxmikanthreddy2002@gmail.com>
2022-10-20 18:30:34 +0530

    first-commit

---
+++ pollutants.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+# 2. Class which describes Bank Notes measurements
+class Pollutants(BaseModel):
+    ph: float
+    Hardness: float
+    Solids: float
+    Chloramines: float
+    Sulfate: float
+    Conductivity: float
+    Organic_carbon: float
+    Trihalomethanes: float
+    Turbidity: float
